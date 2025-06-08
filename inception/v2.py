8143d618d782667f059c3089d7428f9988590753
Mukesh <hey.mukesh272921@gmail.com>
2025-06-08 22:47:05 +0530

    feat: add inception v2

--- inception/v2.py
+++ inception/v2.py
@@ -3,33 +3,41 @@ from torch import nn
 
 from utils import train_model_cifar10
 
-class InceptionV1Block(nn.Module):
+class ConvBNReLU(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU(inplace=True)
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        return x
+
+class InceptionV2Block(nn.Module):
     def __init__(self, arch: list[int], in_channels: int):
         super().__init__()
 
-        self.b1 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=arch[0], kernel_size=1, stride=1, padding=0),
-            nn.ReLU()
-        )
+        self.b1 = ConvBNReLU(in_channels=in_channels, out_channels=arch[0], kernel_size=1, stride=1, padding=0)
 
         self.b2 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=arch[1], kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=arch[1], out_channels=arch[2], kernel_size=3, stride=1, padding=1),
-            nn.ReLU()
+            ConvBNReLU(in_channels=in_channels, out_channels=arch[1], kernel_size=1, stride=1, padding=0),
+            ConvBNReLU(in_channels=arch[1], out_channels=arch[2], kernel_size=3, stride=1, padding=1),
         )
 
         self.b3 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=arch[3], kernel_size=1, stride=1, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=arch[3], out_channels=arch[4], kernel_size=5, stride=1, padding=2),
-            nn.ReLU()
+            ConvBNReLU(in_channels=in_channels, out_channels=arch[3], kernel_size=1, stride=1, padding=0),
+            ConvBNReLU(in_channels=arch[3], out_channels=arch[4], kernel_size=3, stride=1, padding=1),
+            ConvBNReLU(in_channels=arch[4], out_channels=arch[4], kernel_size=3, stride=1, padding=1)
         )
 
         self.b4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(in_channels=in_channels, out_channels=arch[5], kernel_size=1, stride=1, padding=0),
-            nn.ReLU()
+            ConvBNReLU(in_channels=in_channels, out_channels=arch[5], kernel_size=1, stride=1, padding=0),
         )
 
     def forward(self, x):
@@ -44,7 +52,7 @@ class AuxiliaryClassifier(nn.Module):
     def __init__(self, in_channels: int, classes: int):
         super().__init__()
 
-        self.avgpool = nn.AvgPool2d(kernel_size=5, stride=3)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=128, kernel_size=1, padding="same")
         self.fc1 = nn.Linear(in_features=128, out_features=1024)
         self.fc2 = nn.Linear(in_features=1024, out_features=classes)
@@ -61,27 +69,27 @@ class AuxiliaryClassifier(nn.Module):
 
         return x
 
-class GoogLeNet(nn.Module):
+class GoogLeNetV2(nn.Module):
     def __init__(self, classes: int, in_channels: int = 3):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=7, stride=2, padding=3)
+        self.conv1 = ConvBNReLU(in_channels=in_channels, out_channels=64, kernel_size=7, stride=2, padding=3)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1)
+        self.conv2 = ConvBNReLU(in_channels=64, out_channels=64, kernel_size=1, stride=1, padding=0)
+        self.conv3 = ConvBNReLU(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1)
 
-        self.incep_3a = InceptionV1Block([64, 96, 128, 16, 32, 32], 192)
-        self.incep_3b = InceptionV1Block([128, 128, 192, 32, 96, 64], 256)
+        self.incep_3a = InceptionV2Block([64, 96, 128, 16, 32, 32], 192)
+        self.incep_3b = InceptionV2Block([128, 128, 192, 32, 96, 64], 256)
 
-        self.incep_4a = InceptionV1Block([192, 96, 208, 16, 48, 64], 480)
-        self.incep_4b = InceptionV1Block([160, 112, 224, 24, 64, 64], 512)
-        self.incep_4c = InceptionV1Block([128, 128, 256, 24, 64, 64], 512)
-        self.incep_4d = InceptionV1Block([112, 144, 288, 32, 64, 64], 512)
-        self.incep_4e = InceptionV1Block([256, 160, 320, 32, 128, 128], 528)
+        self.incep_4a = InceptionV2Block([192, 96, 208, 16, 48, 64], 480)
+        self.incep_4b = InceptionV2Block([160, 112, 224, 24, 64, 64], 512)
+        self.incep_4c = InceptionV2Block([128, 128, 256, 24, 64, 64], 512)
+        self.incep_4d = InceptionV2Block([112, 144, 288, 32, 64, 64], 512)
+        self.incep_4e = InceptionV2Block([256, 160, 320, 32, 128, 128], 528)
 
-        self.incep_5a = InceptionV1Block([256, 160, 320, 32, 128, 128], 832)
-        self.incep_5b = InceptionV1Block([384, 192, 384, 48, 128, 128], 832)
+        self.incep_5a = InceptionV2Block([256, 160, 320, 32, 128, 128], 832)
+        self.incep_5b = InceptionV2Block([384, 192, 384, 48, 128, 128], 832)
 
         self.aux1 = AuxiliaryClassifier(in_channels=512, classes=classes)
         self.aux2 = AuxiliaryClassifier(in_channels=528, classes=classes)
@@ -90,19 +98,16 @@ class GoogLeNet(nn.Module):
         self.dropout = nn.Dropout(0.4)
         self.fc = nn.Linear(in_features=1024, out_features=classes)
 
-        self.lrn = nn.LocalResponseNorm(size=5)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         outputs = []
 
-        x = self.relu(self.conv1(x))
+        x = self.conv1(x)
         x = self.maxpool(x)
-        x = self.lrn(x)
 
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = self.lrn(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
 
         x = self.maxpool(x)
 
@@ -142,4 +147,4 @@ class GoogLeNet(nn.Module):
 
         return outputs        
 
-train_model_cifar10(lambda: GoogLeNet(classes=10, in_channels=3), 5)
+train_model_cifar10(lambda: GoogLeNetV2(classes=10, in_channels=3), 3)
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
-from functools import partial
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
+
+losses = []
 
 def train_model_cifar10(model_fn, epochs):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -11,34 +12,32 @@ def train_model_cifar10(model_fn, epochs):
     dataset_std = [0.2023, 0.1994, 0.2010]
 
     train_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((64, 64)),
         transforms.RandomHorizontalFlip(),
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.RandomRotation(15),
         transforms.ToTensor(),
         transforms.Normalize(dataset_mean, dataset_std)
     ])
+
     test_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((64, 64)),
         transforms.ToTensor(),
         transforms.Normalize(dataset_mean, dataset_std)
     ])
 
     train_dataset = datasets.CIFAR10(root="data", train=True, transform=train_transform, download=True)
-    test_dataset = datasets.CIFAR10(root="data", train=True, transform=test_transform, download=True) 
+    test_dataset = datasets.CIFAR10(root="data", train=False, transform=test_transform, download=True) 
 
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
 
     model = model_fn().to(device)
     loss_function = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(params=model.parameters())
 
-    model.train()
-
-    losses = []
-
     for epoch in range(epochs):
+        model.train()
         running_loss = 0.
 
         for i, (images, labels) in enumerate(train_loader):
@@ -46,7 +45,11 @@ def train_model_cifar10(model_fn, epochs):
             labels = labels.to(device)
 
             output = model(images)
-            loss = loss_function(output, labels)
+
+            if isinstance(output, list):
+                  loss = 0.1 * loss_function(output[0], labels) + 0.1 * loss_function(output[1], labels) + loss_function(output[2], labels)
+            else:
+                loss = loss_function(output, labels)
 
             optimizer.zero_grad()
             loss.backward()
@@ -59,19 +62,22 @@ def train_model_cifar10(model_fn, epochs):
                 losses.append(running_loss)
                 running_loss = 0
 
+        with torch.no_grad():
+          model.eval()
+          correct = 0
+          total = 0
 
-        model.eval()
-        correct = 0
-        total = 0
+          for i, (images, labels) in enumerate(test_loader):
+              images = images.to(device)
+              labels = labels.to(device)
 
-        for i, (images, labels) in enumerate(test_loader):
-            images = images.to(device)
-            labels = labels.to(device)
+              output = model(images)
+              if isinstance(output, list):
+                output = output[-1]
 
-            output = model(images)
-            _, predicted = torch.max(output, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+              _, predicted = torch.max(output, 1)
+              total += labels.size(0)
+              correct += (predicted == labels).sum().item()
 
-        acc = correct / total
-        print(f"[epoch {epoch + 1}] validation acc - {acc * 100:.3f}%")
+          acc = correct / total
+          print(f"[epoch {epoch + 1}] validation acc - {acc * 100:.3f}%")
